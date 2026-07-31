c76243265b2f1723d2fdcb05ef293b58b6790868
maribukh <mariebukhaidze@gmail.com>
2026-07-31 22:06:51 +0400

    refactor: little changes

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -4,18 +4,15 @@ function About() {
   return (
     <div className="about-page">
       <h1>About Poki Land</h1>
-      <p>
-        Poki Land is a Pokémon search app built while learning React, from class
-        components to hooks and routing.
-      </p>
+      <p>Poki Land is a Pokémon search app.</p>
       <p>
         Built as part of the{' '}
         <a
-          href="https://rs.school/courses/reactjs"
+          href="https://rs.school/courses/short-track"
           target="_blank"
           rel="noreferrer"
         >
-          RS School React course
+          RS JS / Front-end Short Track Course
         </a>
         .
       </p>
