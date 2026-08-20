208af349cd5591041f3080e3d32ec8e7c6ee1764
maribukh <mariebukhaidze@gmail.com>
2026-08-21 00:31:39 +0400

    refactor:rename  t -> item

--- src/pages/PokemonDetails/DetailsTypes.tsx
+++ src/pages/PokemonDetails/DetailsTypes.tsx
@@ -7,9 +7,9 @@ interface DetailsTypesProps {
 function DetailsTypes({ types }: DetailsTypesProps) {
   return (
     <div className="details-card__types">
-      {types.map((t) => (
-        <span key={t.type.name} className="details-card__type-tag">
-          {t.type.name}
+      {types.map((item) => (
+        <span key={item.type.name} className="details-card__type-tag">
+          {item.type.name}
         </span>
       ))}
     </div>
